91bd570a898c5c16d250fdb891c0d9589b23ec59
BELONG1997520 <44727232+BELONG1997520@users.noreply.github.com>
2019-11-21 14:58:36 +0800

    Initial commit

---
+++ springboot_quick/src/main/java/itcast/controller/QuickController.java
@@ -0,0 +1,14 @@
+package itcast.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuickController {
+
+    @RequestMapping("/quick")
+    public String quick(){
+        return "springBoot !!!";
+    }
+
+}
